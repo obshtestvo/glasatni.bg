ae14acd86664fe3f060a260e505bfa8ec89ec7e4
jsibelius <c_c@abv.bg>
2014-08-13 14:08:15 +0300

    Added users show page, action and route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   resources :badges
 
   devise_for :users
+  resources :users, :only => [:show]
+
   resources :proposals
 
   resources :themes
 
-  root "application#home"
+  root "themes#index"
 end
