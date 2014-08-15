ce67a59c9764086e0d809155b3e1517d7f8905f6
jsibelius <c_c@abv.bg>
2014-08-15 13:50:42 +0300

    Fixed index and create actions in comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,20 @@
 class CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.where(proposal_id: comment_params[:proposal_id])
+    #TODO hide stuff
+    render json: @comments
+  end
+
   def create
     @comment = Comment.new(comment_params)
+    p comment_params
     @comment.user = current_user
 
-    respond_to do |format|
-      if @comment.save
-        #format.html { redirect_to @comment, notice: 'comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        #format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      render json: { status: :created }
+    else
+      render json: @comment.errors, status: :unprocessable_entity
     end
   end
 
@@ -43,7 +46,7 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:theme_id, :title, :content, :up, :down)
+      params.require(:comment).permit(:theme_id, :proposal_id, :title, :content, :up, :down)
     end
 
 end
