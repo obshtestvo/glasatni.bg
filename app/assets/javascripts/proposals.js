57744624ae867fd98bc36885d814405d8fb57883
jsibelius <c_c@abv.bg>
2014-08-25 01:48:40 +0300

    Made angular and turbolinks work nicely together.

--- app/assets/javascripts/proposals.js
+++ app/assets/javascripts/proposals.js
@@ -2,3 +2,6 @@ promeni.controller('ProposalController', ['$scope', function($scope) {
   $scope.contacts = ["test test test", "more tests"];
 }]);
 
+$(document).on('ready page:load', function() {
+  angular.bootstrap('body', ['promeni'])
+})
