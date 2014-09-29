5a0180944e5505d014ed5139893d4d5143be6726
jsibelius <c_c@abv.bg>
2014-09-29 23:17:51 +0300

    Changed default_url path for avatars.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   devise :omniauthable, :omniauth_providers => [:facebook]
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    :default_url => "/images/:style/missing.png"
+    :default_url => "/system/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   #validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
 
