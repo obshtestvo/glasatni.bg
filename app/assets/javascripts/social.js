e023b9339290e05da2ea293bd511ddbc16019bc1
jsibelius <c_c@abv.bg>
2014-10-15 15:43:56 +0300

    Fixed bug with fb socialbox not loading sometimes.

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -29,7 +29,7 @@ var loadFBSDK = function() {
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
+     js.src = "//connect.facebook.net/en_US/all.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 }
