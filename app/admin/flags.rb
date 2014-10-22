04cd5522a5d4d4aa550894e3d173c73973623f78
jsibelius <c_c@abv.bg>
2014-10-23 00:59:41 +0300

    Fixed: bug in admin panel regarding path to flaggable.

--- app/admin/flags.rb
+++ app/admin/flags.rb
@@ -15,9 +15,9 @@ ActiveAdmin.register Flag do
     column(:offender) { |f| link_to f.flaggable.user.name, admin_user_path(f.flaggable.user) }
     column(:flaggable) do |f|
       if f.flaggable_type == "Comment"
-        link_to f.flaggable.content, admin_proposal_comment_path(f)
+        link_to f.flaggable.content, admin_proposal_comment_path(f.flaggable)
       else
-        link_to f.flaggable.title, admin_proposal_path(f)
+        link_to f.flaggable.title, admin_proposal_path(f.flaggable)
       end
     end
   end
