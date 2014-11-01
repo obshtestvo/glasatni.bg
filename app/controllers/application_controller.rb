99d0dc6e2a967fb719a8c4b507cbbfba0a006480
jsibelius <c_c@abv.bg>
2014-11-01 17:36:20 +0200

    Removed basic http authorization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with :name => ENV['http_username'], :password => ENV['http_password']
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
