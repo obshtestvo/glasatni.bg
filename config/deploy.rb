37c276f081406aef94b0354c017030fafd8e30bc
Dimitar Dimitrov <me@ddimitrov.name>
2014-11-10 18:23:00 +0200

    Fix deploy.rb options (squash)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 lock '3.2.1'
 
-set :application,     'glasatni.bg'
+set :application,     'glasatni'
 set :repo_url,        'https://github.com/obshtestvo/glasatni.bg.git'
-set :linked_files,    %w(config/database.yml config/secrets.yml config/settings.yml)
+set :linked_files,    %w(config/database.yml config/secrets.yml config/application.yml)
 set :linked_dirs,     %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 set :keep_releases,   20
 set :rails_env,       'production'
