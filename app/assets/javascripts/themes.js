cb952c8353ab01be3af92cf3066a25ecfc266afe
jsibelius <c_c@abv.bg>
2014-11-19 21:30:50 +0200

    Info for themes is loaded via angular in home page.

--- app/assets/javascripts/themes.js
+++ app/assets/javascripts/themes.js
@@ -1,9 +1,20 @@
-promeni.controller("ThemeController", ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http) {
+promeni.controller("ThemeShowController", ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http) {
 
   $http.get("/api/v1/themes/" + $routeParams.id).success(function(theme) {
     $scope.theme = theme;
   });
 
   $scope.proposal = [];
+
+}]);
+
+promeni.controller("ThemeIndexController", ["$scope", "$http", function($scope, $http) {
+
+  $scope.order = ["education", "participation", "volunteering", "ngo"];
+
+  $http.get("/api/v1/themes").success(function(themes) {
+    $scope.meta = themes.filter(function(t) { return t.en_name === "meta" })[0];
+    $scope.themes = themes.filter(function(t) { return t.en_name !== "meta" });
+  });
 
 }]);
--- app/views/api/v1/themes/index.json.jbuilder
+++ app/views/api/v1/themes/index.json.jbuilder
@@ -1,1 +1,8 @@
-json.array! @themes, :id, :name
+json.array! @themes do |t|
+  json.id t.id
+  json.name t.name
+  json.info t.info
+  json.en_name Theme.en_names.key(t.name)
+end
+
+
