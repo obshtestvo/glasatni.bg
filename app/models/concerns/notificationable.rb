c49d6727ee7759c8d6c4f264e5b1a4cbdda45dc9
jsibelius <c_c@abv.bg>
2014-12-10 21:12:19 +0200

    Fixed: added notificationable.rb file to git.

---
+++ app/models/concerns/notificationable.rb
@@ -0,0 +1,21 @@
+module Notificationable
+  extend ActiveSupport::Concern
+
+  def create_notification action
+
+    case action
+      when :comment_created then proposal = self.commentable; recipient = self.commentable.theme.moderator
+      when :proposal_created then proposal = self; recipient = self.theme.moderator
+    end
+
+    Notification.create(
+        {
+            user: self.user,
+            proposal: proposal,
+            recipient: recipient,
+            action: Notification.actions[action]
+        }
+    )
+  end
+
+end
