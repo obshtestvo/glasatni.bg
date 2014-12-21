4fc89456e5741edf54d0190cee40a22aff0e0601
jsibelius <c_c@abv.bg>
2014-12-21 17:29:51 +0200

    Introduced AuthService and NavigationController.

--- app/assets/javascripts/authentication.js
+++ app/assets/javascripts/authentication.js
@@ -1,13 +1,34 @@
-glasatni.controller("LoginController", ["$scope", "$http", function($scope, $http) {
+glasatni.service("AuthService", ["$http", function($http) {
+  var _user = null;
 
-  $scope.login = function(email, password, remember_me) {
-
+  this.login = function(email, password, remember_me) {
     $http.post("/users/sign_in.json", {
       user: { email: email, password: password, remember_me: remember_me }
-    }).then(function(res) {
-      $scope.user = res.data;
+    }).then(function (res) {
+      _user = res.data;
     });
-
   };
+
+  this.getUser = function() {
+    return _user;
+  };
+
+  this.logout = function() {
+    $http.post("/users/sign_out.json", {}).then(function() {
+      _user = null;
+    });
+  };
+
+}]);
+
+glasatni.controller("LoginController", ["$scope", "$http", "AuthService", function($scope, $http, AuthService) {
+
+  $scope.AuthService = AuthService;
+
+}]);
+
+glasatni.controller("NavigationController", ["$scope", "AuthService", function($scope, AuthService) {
+
+  $scope.AuthService = AuthService;
 
 }]);
