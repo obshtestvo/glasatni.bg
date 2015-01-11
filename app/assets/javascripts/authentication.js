4c6322b870dd749f2c8252d947c695247e66a7e4
jsibelius <c_c@abv.bg>
2015-01-12 01:20:45 +0200

    Added angularized ‘register’ page.

--- app/assets/javascripts/authentication.js
+++ app/assets/javascripts/authentication.js
@@ -3,12 +3,17 @@ glasatni.service("AuthService", ["$http", "$q", "$location", "messageService", f
   var d = $q.defer();
 
   this.login = function(email, password, rememberMe, redirectTo) {
+    messageService.removeByLocation('login');
     $http.post("/users/sign_in.json", {
       user: { email: email, password: password, remember_me: rememberMe }
     }).then(function (res) {
       _user = res.data;
 
-      messageService.removeByLocation('login');
+      messageService.push({
+        msg: "Успешно влязохте в своя профил.",
+        type: "success",
+        destination: "top message"
+      });
 
       if(redirectTo) {
         $location.path(redirectTo);
@@ -25,6 +30,40 @@ glasatni.service("AuthService", ["$http", "$q", "$location", "messageService", f
         destination: "login"
       });
 
+    });
+  };
+
+  this.register = function(email, name, password, passwordConfirmation, bio, subscribed, redirectTo) {
+
+    messageService.removeByLocation('register');
+
+    $http.post("/users.json", {
+      user: { email: email, name: name, password: password, password_confirmation: passwordConfirmation, bio: bio, subscribed: subscribed }
+    }).then(function(res) { // on success
+
+      messageService.push({
+        msg: "Регистрацията приключи успешно",
+        type: "success",
+        destination: "top message"
+      });
+
+      _user = res.data;
+
+      if (redirectTo) {
+        $location.path(redirectTo);
+      }
+
+    }, function(response) { // on error
+
+      for(wrongField in response.data.errors) {
+
+        messageService.push({
+          msg: wrongField + " " + response.data.errors[wrongField],
+          type: "danger",
+          destination: "register"
+        });
+
+      }
     });
   };
 
