6dc613e5e6823bb26317d99751b9be093f407189
jsibelius <c_c@abv.bg>
2015-01-11 13:09:24 +0200

    Fixed the ‘more button’ for notifications.

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,9 +1,15 @@
-glasatni.controller("NotificationIndexController", ["$scope", "$http", function($scope, $http) {
+glasatni.factory('Notification', ['$resource', function($resource) {
+  return $resource('/api/v1/notifications/:id', null, {
+    'query': { method:'GET', isArray: false }
+  });
+}]);
 
-  $http.get("/api/v1/notifications").then(function(res) {
-    $scope.notifications = res.data.notifications;
-    $scope.page = res.data.page;
-    $scope.notificationsCount = res.data.notifications_count;
+glasatni.controller("NotificationIndexController", ["$scope", "Notification", function($scope, Notification) {
+
+  Notification.query({ page: 1 }).$promise.then(function(res) {
+    $scope.notifications = res.notifications;
+    $scope.page = res.page;
+    $scope.notificationsCount = res.notifications_count;
   });
 
 }]);
