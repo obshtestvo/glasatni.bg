1353aa7240b3f96681c59e0d93a83cf179701527
jsibelius <c_c@abv.bg>
2015-02-17 23:23:00 +0200

    Add basic logic for statuses

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,5 @@
 class Status < ActiveRecord::Base
   belongs_to :proposal, dependent: :destroy
 
-  enum kinds: [:discussion, :rejected_no_reason, :rejected_with_reason, :accepted, :in_force]
+  enum kinds: [:approved, :rejected_no_reason, :rejected_with_reason, :accepted, :in_force]
 end
