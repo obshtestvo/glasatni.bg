7f724c4a7bfb93bb270756eb436c5b8801ba8333
jsibelius <c_c@abv.bg>
2015-02-25 22:00:30 +0200

    Add 2 more statuses for proposals.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,5 @@
 class Status < ActiveRecord::Base
   belongs_to :proposal, dependent: :destroy
 
-  enum kinds: [:approved, :rejected_no_reason, :rejected_with_reason, :accepted, :in_force]
+  enum kinds: [:approved, :submitted, :not_submitted, :rejected_no_reason, :rejected_with_reason, :accepted, :in_force]
 end
