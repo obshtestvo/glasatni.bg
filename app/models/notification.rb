e9638e96cab22825596a0b6107e0ae05379ec82e
Petko Tsikov <tsikov@gmail.com>
2016-02-01 11:51:25 +0200

    Fix bug regarding notifications.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ActiveRecord::Base
 
-  belongs_to :recipient, class: User
+  belongs_to :recipient, class_name: :User, foreign_key: :user_id
   belongs_to :user
   belongs_to :proposal
 
